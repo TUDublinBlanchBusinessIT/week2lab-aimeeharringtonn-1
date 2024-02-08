d54ffc5e627931007ac977cd9c3c085bf79f1d8a
aimeeharringtonn-1 <b00135854@mytudublin.ie>
2024-02-08 01:43:19 +0000

    added printSteps() function

---
+++ partA.py
@@ -0,0 +1,15 @@
+#Aimee Harrington
+#07/02/2024
+#A Program to Print Steps 1 to 9 to the Screen
+def printSteps():
+    print("step 4")
+    print("step 5")
+    print("step 6")
+
+printSteps()
+print("Step 1")
+print("Step 2")
+print("Step 3")
+print("Step 7")
+print("Step 8")
+print("Step 9")
---
+++ partB.py
@@ -0,0 +1,9 @@
+#Aimee Harrington
+#07/02/2024
+#B Program to Print Steps 1 to 9 variable
+def printSteps(a, b):
+    for i in range(a, b + 1):
+        print("Step", i)
+
+printSteps(1, 9)
+
